8fc7c1a67e133f0cc01f137ae8e33cf11efdfd31
GinoAntonel <ginoantonel.ga@gmail.com>
2019-11-26 22:11:21 -0300

    Cambios importantes xd

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class MyCustomFormState extends State<MyCustomForm> {
   TextEditingController _fecha = new TextEditingController();
 
   var fecha = new DateTime.now();
-  var dateFormat = DateFormat("yMd");
+  var dateFormat = DateFormat("Hms");
 
 
 
@@ -68,21 +68,25 @@ class MyCustomFormState extends State<MyCustomForm> {
       double valueC = data.snapshot.value as double;
       double valueP = data.snapshot.value as double;
       String strDate = dateFormat.format(fecha).toString();
+      double valuePq = data.snapshot.value as double;
     });
   }
 
-  saveOnChanged(String valueName, String valueA, double valueC, double valueP, String strDate) async {
+  saveOnChanged(String valueName, String valueA, double valueC, double valueP, String strDate,valuePq) async {
     await database.set({
       'nombre': valueName,
       'apellido': valueA,
       'cantidad': valueC,
       'precio': valueP,
       'fecha': strDate,
+      'paquete': valuePq,
     });  
   }
+  bool tenSelected = false;
+  bool twSelected = false;
+
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
       child: ListView(
@@ -120,6 +124,53 @@ class MyCustomFormState extends State<MyCustomForm> {
                 return null;
               },
             ),
+          ),
+          Column(
+            children: [
+              Divider(),
+              Text('Tamanio de la etiqueta'),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ChoiceChip( 
+                      labelPadding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+                      label: Text("10"),
+                      selected: tenSelected,
+                      onSelected: (selected) {
+                        if (tenSelected == false){
+                          setState(() {
+                            tenSelected = true;
+                            twSelected = false;
+                          });
+                        }
+                      } 
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ChoiceChip( 
+                      labelPadding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+                      label: Text("20"),
+                      selected: twSelected,
+                      onSelected: (selected) {
+                        if (twSelected == false){
+                          setState(() {
+                            twSelected = true;
+                            tenSelected = false;
+                          });
+                        }
+                      } 
+                    ),
+                  ),
+                  ]
+         ),
+              ),
+            Divider(),
+            ],
           ),
           Padding(
             padding:
@@ -170,7 +221,8 @@ class MyCustomFormState extends State<MyCustomForm> {
                   double cantidad = double.parse(_cantidad.value.text);
                   double precio = double.parse(_precio.value.text);
                   DateTime fechaF = new DateTime.now();
-                  saveOnChanged(nombre, apellido, cantidad, precio, fechaF.toString());
+                  var valuePq = tenSelected ? 10 : 20;
+                  saveOnChanged(nombre, apellido, cantidad, precio, fechaF.toString(), valuePq);
                 }
               },
               child: Text('Empezar'),
@@ -190,6 +242,7 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidget extends State<MyWidget> {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -197,33 +250,56 @@ class _MyWidget extends State<MyWidget> {
         length: 3,
         child: Scaffold(
           appBar: AppBar(
+            backgroundColor: Colors.green,
             bottom: TabBar(
+              indicator: UnderlineTabIndicator(
+                borderSide: BorderSide(
+                  width: 5.0,
+                  color: Colors.greenAccent[400]
+                ),
+              ),
               tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.access_time)),
                 Tab(icon: Icon(Icons.directions_transit)),
                 Tab(icon: Icon(Icons.directions_bike)),
               ],
             ),
-            title: Text('Tabs Demo'),
+            title: Text('Quit Smoking'),
           ),
           body: TabBarView(
             children: [
               Container(
-                child: (
-                Row(
-                  children: <Widget>[
-                    Text(
-                    'Hello, d How are you?',
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                child: SizedBox(
+                  height: 100,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ListView(
+                      children: [
+                        Container (
+                          padding: EdgeInsets.only(right: 5, left: 5, top: 5, bottom: 5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15.0)),
+                          child: MyCard()
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: 10, left: 10, top: 5, bottom: 5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15.0)),
+                          child: TimeCard(),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(right: 10, left: 10, top: 5, bottom: 5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15.0)),
+                          child: Probando (),
+                        )
+                      ],
                     ),
-                  ],
-                )
-                )
+                  ),
+                ),
+                
               ),
-
-              Icon(Icons.directions_transit),
+              StepperPage(),
               Icon(Icons.directions_bike),
             ],
           ),
@@ -231,4 +307,411 @@ class _MyWidget extends State<MyWidget> {
       ),
     );
   }
+}
+
+class StepperPage extends StatefulWidget {
+  @override
+  _StepperState createState() => _StepperState();
+}
+
+class _StepperState extends State<StepperPage> {
+
+  final database = FirebaseDatabase.instance.reference();
+  final _nombre = new TextEditingController();
+
+  StreamSubscription _subscription;
+
+  void initState() {
+    super.initState();
+    _subscription = database.onValue.listen((data) {
+      String valueNombre = data.snapshot.value as String ?? "";
+    });
+  }
+
+  saveOnChanged(String valueNombre) async {
+    await database.set({
+      'nombre': valueNombre,
+    });  
+  }
+  List<Step> steps = [
+
+    Step(
+      title: const Text('Nombre y Apellido'),
+      isActive: true,
+      state: StepState.complete,
+      content: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Nombre'),
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Password'),
+          ),
+        ],
+      ),
+    ),
+    Step(
+      isActive: false,
+      state: StepState.editing,
+      title: const Text('Address'),
+      content: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Home Address'),
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Postcode'),
+          ),
+        ],
+      ),
+    ),
+    Step(
+      state: StepState.error,
+      title: const Text('Avatar'),
+      subtitle: const Text("Error!"),
+      content: Column(
+        children: <Widget>[
+          CircleAvatar(
+            backgroundColor: Colors.red,
+          )
+        ],
+      ),
+    ),
+  ];
+
+  StepperType stepperType = StepperType.vertical;
+
+  int currentStep = 0;
+  bool complete = false;
+
+  next(){
+    var nombre = _nombre.text;
+    saveOnChanged(nombre);
+    currentStep + 1 != steps.length
+      ? goTo(currentStep + 1)
+      : setState(() => complete = true);
+  }
+
+  cancel(){
+    if(currentStep > 0){
+      goTo(currentStep - 1);
+    }
+  }
+
+  goTo(int step){
+    setState(() => currentStep = step);
+  }
+
+  switchStepType(){
+    setState(() => stepperType == StepperType.horizontal
+      ? stepperType = StepperType.vertical
+      : stepperType = StepperType.horizontal
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: AppBar(
+          title: Text('Editar Cuenta'),
+          backgroundColor: Colors.green,
+        ),
+        body: Column(
+          children: <Widget>[
+            complete ? Expanded(
+                    child: Center(
+                      child: AlertDialog(
+                        title: new Text("Profile Created"),
+                        content: new Text(
+                          "Tada!",
+                        ),
+                        actions: <Widget>[
+                          new FlatButton(
+                            child: new Text("Close"),
+                            onPressed: () {
+                              setState(() => complete = false);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                : Expanded(
+                    child: Stepper(
+                      type: stepperType,
+                      steps: steps,
+                      currentStep: currentStep,
+                      onStepContinue: next,
+                      onStepTapped: (step) => goTo(step),
+                      onStepCancel: cancel,
+                    ),
+                  ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.autorenew),
+          onPressed: switchStepType,
+        ),
+        );
+  }
+}
+
+class MyCard extends StatefulWidget {
+
+  MyCard({Key key}) : super(key: key);
+
+  @override
+  _MyCard createState() => _MyCard();
+}
+
+class _MyCard extends State<MyCard> {
+  final database = FirebaseDatabase.instance.reference();
+  var nombre;
+  var fecha;
+  var diferencia;
+  var tiempo;
+  Timer timer;
+  var dateFormat = DateFormat("Hms");
+
+
+  void calculateDiference(){
+    database.once().then((DataSnapshot snapshot) {
+
+      setState(() {
+        nombre = snapshot.value['nombre'];
+        fecha = DateTime.parse(snapshot.value['fecha']);
+        diferencia = new DateTime.now().difference(fecha);
+      });
+    });
+  }
+
+  void initState(){
+
+    timer = Timer.periodic(Duration(seconds: 1), (Timer t) => calculateDiference());
+    super.initState();
+  }
+@override
+Widget build(BuildContext context) {
+  return Center(
+    child: Card(
+      shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(30.0)),
+      child: Container(
+        padding: EdgeInsets.only(bottom: 16,top: 16),
+        decoration: new BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0), color: Colors.lightGreen),
+         
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center, 
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0), color: Colors.lightGreen
+            ), 
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Image.asset("assets/images/time.png", width: 70,),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0), color: Colors.lightGreen
+            ),
+            padding: EdgeInsets.all(10),
+            child: Text(
+              diferencia.toString(),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  color: Colors.grey[800],
+                  fontWeight: FontWeight.w900,
+                  fontStyle: FontStyle.italic,
+                  fontFamily: 'Open Sans',
+                  fontSize: 20),
+            ),
+          )],
+      ),
+      ),
+    ),
+  );
+}
+}
+
+class TimeCard extends StatefulWidget {
+  TimeCard({Key key}) : super(key: key);
+
+  @override
+  _TimeCard createState() => _TimeCard();
+}
+
+class _TimeCard extends State<TimeCard> {
+  final database = FirebaseDatabase.instance.reference();
+  var cantidad;
+  Timer timer;
+  var fecha;
+  var diferencia = 1;
+  var paquete;
+  var cigarrillos = 0;
+  double ahorro = 0;
+
+  void calculateC(){
+      database.once().then((DataSnapshot snapshot){
+        fecha = DateTime.parse(snapshot.value['fecha']);
+        cantidad = snapshot.value['cantidad'];
+        paquete = snapshot.value['paquete'];
+      setState(() {
+        if (DateTime.now().difference(fecha).inHours >=  diferencia){
+          diferencia += 1;
+          cigarrillos = paquete * cantidad;
+          ahorro += cigarrillos / 24;
+        }
+      });
+      print(ahorro);
+      });
+  }
+
+
+  void initState(){
+    timer = Timer.periodic(Duration(seconds: 1), (Timer t) => calculateC());
+    super.initState();
+  }
+
+  @override
+Widget build(BuildContext context) {
+  return Center(
+    child: Card(
+      shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(30.0)),
+      child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0), color: Colors.lightGreen
+            ),
+         padding: EdgeInsets.only(bottom: 16,top: 16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center, 
+        children: <Widget>[
+          Container( 
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0), color: Colors.lightGreen
+            ),
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Image.asset("assets/images/stop-smoking.png", width: 70,),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0), color: Colors.lightGreen
+            ),
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Cigarrillos no fumados: ' + ahorro.toStringAsFixed(0),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  color: Colors.grey[800],
+                  fontWeight: FontWeight.w900,
+                  fontStyle: FontStyle.italic,
+                  fontFamily: 'Open Sans',
+                  fontSize: 20),
+            ),
+          )
+        ],
+      ),
+      ),
+    ),
+  );
+}
+}
+
+
+class Probando extends StatefulWidget {
+  Probando({Key key}) : super(key: key);
+
+  @override
+  _Probando createState() => _Probando();
+}
+
+class _Probando extends State<Probando> {
+  final database = FirebaseDatabase.instance.reference();
+  var precio;
+  var cantidad;
+  double ahorro =0;
+  Timer timer;
+  var fecha;
+  var diferencia = 60;
+  var sumar;
+  String s = "\$" ;
+
+  void calculateSave(){
+      database.once().then((DataSnapshot snapshot){
+        fecha = DateTime.parse(snapshot.value['fecha']);
+        precio = snapshot.value['precio'];
+        cantidad = snapshot.value['cantidad'];
+      setState(() {
+        sumar = (precio * cantidad) / 24;
+        if (DateTime.now().difference(fecha).inMinutes >=  diferencia){
+          diferencia += 60;
+          ahorro += sumar;
+        }
+      });
+      });
+  }
+
+
+  void initState(){
+    timer = Timer.periodic(Duration(seconds: 1), (Timer t) => calculateSave());
+    super.initState();
+    
+  }
+  @override
+  Widget build(BuildContext context) {
+
+  return Center(
+    child: Card(
+      shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(30.0)),
+      child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0), color: Colors.lightGreen
+            ),
+         padding: EdgeInsets.only(bottom: 11,top: 11),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center, 
+        children: <Widget>[
+          Container( 
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0), color: Colors.lightGreen
+            ),
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Image.asset("assets/images/ahorrar.png", width: 70,),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0), color: Colors.lightGreen
+            ),
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Dinero ahorrado: ' + ahorro.toStringAsFixed(0) + ' $s', 
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  color: Colors.grey[800],
+                  fontWeight: FontWeight.w900,
+                  fontStyle: FontStyle.italic,
+                  fontFamily: 'Open Sans',
+                  fontSize: 20),
+            ),       
+          )
+        ],
+      ),
+      ),
+    ),
+  );
+}
 }
